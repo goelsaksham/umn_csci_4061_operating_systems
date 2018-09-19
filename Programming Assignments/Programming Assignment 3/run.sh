0cc8be1e066d97e3fd06c739f18a8f1f5c449ece
Saksham Goel <goelx029@umn.edu>
2018-09-18 22:05:24 -0500

    Moving stuff from previous git repository

---
+++ Programming Assignments/Programming Assignment 3/run.sh
@@ -0,0 +1,91 @@
+echo "Cleaning the executables"
+make clean
+echo "Making the executables"
+make
+echo "***************************************************************************"
+echo "TestCase01:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase01/input.txt TestCases/TestCase01/inputdir/ TestCases/TestCase01/outputdir/
+cat TestCases/TestCase01/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase02:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase02/input.txt TestCases/TestCase02/inputdir/ TestCases/TestCase02/outputdir/
+cat TestCases/TestCase02/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase03:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase03/input.txt TestCases/TestCase03/inputdir/ TestCases/TestCase03/outputdir/
+cat TestCases/TestCase03/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase04:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase04/input.txt TestCases/TestCase04/inputdir/ TestCases/TestCase04/outputdir/
+echo "***************************************************************************"
+echo "TestCase05:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase05/input.txt TestCases/TestCase05/inputdir/ TestCases/TestCase05/outputdir/
+cat TestCases/TestCase05/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase06:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase06/input.txt TestCases/TestCase06/inputdir/ TestCases/TestCase06/outputdir/
+cat TestCases/TestCase06/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase07:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase07/input.txt TestCases/TestCase07/inputdir/ TestCases/TestCase07/outputdir/
+cat TestCases/TestCase07/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase08:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase08/input.txt TestCases/TestCase08/inputdir/ TestCases/TestCase08/outputdir/
+cat TestCases/TestCase08/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase09:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase09/input.txt TestCases/TestCase09/inputdir/ TestCases/TestCase09/outputdir/
+cat TestCases/TestCase09/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase10:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase10/input.txt TestCases/TestCase10/inputdir/ TestCases/TestCase10/outputdir/
+echo "***************************************************************************"
+echo "TestCase11:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase11/input.txt TestCases/TestCase11/inputdir/ TestCases/TestCase11/outputdir/
+echo "***************************************************************************"
+echo "TestCase12:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase12/input.txt TestCases/TestCase12/inputdir/ TestCases/TestCase12/outputdir/
+cat TestCases/TestCase12/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase13:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase13/input.txt TestCases/TestCase13/inputdir/ TestCases/TestCase13/outputdir/
+cat TestCases/TestCase13/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase14:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase14/input.txt TestCases/TestCase14/inputdir/ TestCases/TestCase14/outputdir/ 2
+cat TestCases/TestCase14/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase15:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase15/input.txt TestCases/TestCase15/inputdir/ TestCases/TestCase15/outputdir/
+cat TestCases/TestCase15/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase16:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase16/input.txt TestCases/TestCase16/inputdir/ TestCases/TestCase16/outputdir/
+cat TestCases/TestCase16/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase17:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase17/input.txt TestCases/TestCase17/inputdir/ TestCases/TestCase17/outputdir/
+cat TestCases/TestCase17/outputdir/Who_Won/Who_Won.txt
+echo "***************************************************************************"
+echo "TestCase18:"
+echo "***************************************************************************"
+./votecounter TestCases/TestCase18/input.txt TestCases/TestCase18/inputdir/ TestCases/TestCase18/outputdir/ 2
+cat TestCases/TestCase18/outputdir/Who_Won/Who_Won.txt
